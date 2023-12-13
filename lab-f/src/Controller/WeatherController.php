238e5fa0775807d85644154803d8e6dadc43e9fe
bagdax777 <sa48938@zut.edu.pl>
2023-12-13 15:42:02 +0100

    lab-f

---
+++ lab-f/src/Controller/WeatherController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Location;
+use App\Service\WeatherUtil;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WeatherController extends AbstractController
+{
+    #[Route('/weather/{country}/{city}', name: 'app_weather', requirements: ['id' => '\d+'])]
+    public function city(
+        #[MapEntity(mapping: ['country' => 'country', 'city' => 'city'])]
+        Location $location,
+        WeatherUtil $util,
+    ): Response
+    {
+        $measurements = $util->getWeatherForLocation($location);
+
+        return $this->render('weather/city.html.twig', [
+            'location' => $location,
+            'measurements' => $measurements,
+        ]);
+    }
+}
